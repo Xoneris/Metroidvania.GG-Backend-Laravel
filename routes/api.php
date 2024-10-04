a116ed66d30a7be6be03aa870d89a340422ae6a0
Xoneris <kely42@gmx.ch>
2024-10-04 05:11:37 +0200

    Now there is a put and a patch route. Both still need validation tho

--- routes/api.php
+++ routes/api.php
@@ -65,9 +65,50 @@ Route::put('/games/{id}', function (Request $request, $id) {
         return response()->json(['error' => 'Game with this id doesnt exist'], 404);
     }
     // $validatedGame = $request->validate([
+//     'name' => 'required|string',
+//     'slug' => 'required|string',
+//     'developer' => 'required|string',
+//     'publisher' => 'required|string',
+//     'release_window' => 'string|null',
+//     'release_date' => 'date|null',
+//     'description' => 'required|string',
+//     'demo' => 'required|boolean',
+//     'earlyaccess' => 'required|boolean',
+//     'kickstarter_status' => 'string|null',
+//     'kickstarter_page' => 'string|null',
+//     'steam' => 'string|null',
+//     'epic' => 'string|null',
+//     'gog' => 'string|null',
+//     'playstation' => 'string|null',
+//     'xbox' => 'string|null',
+//     'nintendo' => 'string|null',
+//     'twitter' => 'string|null',
+//     'facebook' => 'string|null',
+//     'instagram' => 'string|null',
+//     'tiktoc' => 'string|null',
+//     'youtube' => 'string|null',
+//     'discord' => 'string|null',
+//     'homepage' => 'string|null',
+// ]);
+    
+    $game->update($request->all());
+    return response()->json(['message' => 'Game updated!', 'game' => $game], 200);
+    // return $request;
+    // return response()->json($game);
+    
+
+})->middleware('auth:sanctum');
+
+Route::patch('/games/{id}', function (Request $request, $id) {
+
+    $game = Game::find($id);
+    if (!$game) {
+        return response()->json(['error' => 'Game with this id doesnt exist'], 404);
+    }
+    // $validatedGame = $request->validate([
     //     'name' => 'required|string',
-    //     'release_window' => 'string|null',
-    //     'release_date' => 'date|null',
+    //     'release_window' => 'string',
+    //     'release_date' => 'date',
     //     'demo' => 'required|boolean',
     //     'earlyaccess' => 'required|boolean',
     //     'kickstarter_status' => 'string|null',
